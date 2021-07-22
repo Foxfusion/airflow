1c072f531a4cf620cef65b66f5c9fd7907174b5d
William Fox <wgfox@foxfusion.net>
2021-07-22 12:52:33 -0600

    Create Fox_dag_one.py

---
+++ Fox_dag_one.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from airflow import DAG
+
+from airflow.operators.python_operator import PythonOperator
+
+
+def create_dag(dag_id,
+				schedule,
+				dag_number,
+				default_args):
+
+		def hello_world_py(*args):
+			print('Hello World')
+			print('This is DAG: {}.format(str(dag_number')
+
+		dag = DAG(dag_id,
+					schedule_interval=schedule,
+					default_args=default_args)
+
+		with dag:
+			t1 = PythonOperator(
+				tsk_id='helloworld',
+				python_callable=hello_world_py,
+				dag_number=dag_number)
+
+	return dag
+
+
+	# buikld dag for each number in range(10)
+
+	for n in range(1,10):
+		dag_id = 'hello_world_{}'.format(str(n))
+
+	default_args = {'owner': 'airflow',
+					'satrt_date': datetime(2018, 1, 1)
+	}
+
+
+	schedule = '@daily'
+
+	dag_number = n
+
+	globals()[dag_id] = create_dag(dag_id
+									schedule,
+									dag_number,
+									default_args)
+	
